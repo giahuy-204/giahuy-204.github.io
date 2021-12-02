ced690afa4b3508c1c7681ad8b34042fa89079c6
Huy Dang <giahuy.dng@gmail.com>
2021-12-02 18:27:48 +0700

    login, register, user profile (not done)

--- script/login.js
+++ script/login.js
@@ -1,25 +1,43 @@
+var jwt = localStorage.getItem("jwt");
+if (jwt != null) {
+    alert ('Logged in, redirect you to profile page instead!')
+    window.location.href = 'user_profile.html'
+}
+
 function loginFunction() {
-    var username = "admin";
-    var password = "admin";
-    if(document.getElementById("username").value === username) {
-    if (document.getElementById("password").value === password) {           
-        alert('Login successfully');
-        return true;
-        // jQuery(window).load(function() {
-        // sessionStorage.setItem('status', 'loggedIn')
-        // });
-    }
-    else {
-        document.getElementById("validation_password").innerHTML = "Wrong password!";
-        document.getElementById("password").style.border='2px solid red';
-        document.getElementById("validation_username").innerHTML = "";
-        document.getElementById("username").style.border='1px solid currentColor';
-        return false;
-    }
-    }
-    else {
-        document.getElementById("validation_username").innerHTML = "Wrong username!";
-        document.getElementById("username").style.border='2px solid red';
-        return false;
-    }
+    const email = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "https://herokutuan.herokuapp.com/auth/sign_in");
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.send(JSON.stringify({
+        "email": email,
+        "password": password
+    }));
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          const objects = JSON.parse(this.responseText);
+          console.log(objects);
+            localStorage.setItem("jwt", objects['Access-Token']);
+            Swal.fire({
+              text: objects['message'],
+              icon: 'success',
+              confirmButtonText: 'OK'
+            }).then((result) => {
+              if (result.isConfirmed) {
+                window.location.href = 'index.html';
+              }
+            });
+        //   } else {
+        //     Swal.fire({       
+        //       text: objects['message'],
+        //       icon: 'error',
+        //       confirmButtonText: 'OK'
+        //     });
+        //   }
+        }
+      };
+    return false;
 }
--- script/register.js
+++ script/register.js
@@ -1,11 +1,35 @@
 function registerFunction() {
-    if (document.getElementById("username").value == "") {
+    const email = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "https://herokutuan.herokuapp.com/auth");
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.send(JSON.stringify({
+        "email": email,
+        "password": password
+    }));
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          const objects = JSON.parse(this.responseText);
+          if (document.getElementById("username").value == "") {
         document.getElementById("validation_username").innerHTML = "Please fill in Username!";
         document.getElementById("username").style.border='2px solid red';
         return false;
     } else {
         document.getElementById("validation_username").innerHTML = "";
         document.getElementById("username").style.border='1px solid currentColor';
+        console.log(objects);
+        Swal.fire({
+          text: objects['message'],
+          icon: 'success',
+          confirmButtonText: 'OK'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.location.href = 'login.html';
+          }
+        });
     }
 
     if (document.getElementById("password").value == "") {
@@ -32,4 +56,9 @@ function registerFunction() {
         document.getElementById("confirm_password").style.border='2px solid red';
         return false;
     }
+         
+        }
+      };
+      return false
+    
 }
