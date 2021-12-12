86705bae9a2f61428a9e255d007eab6c43dab02e
Huy Dang <giahuy.dng@gmail.com>
2021-12-12 19:30:35 +0700

    auth if object return null

--- script/make_task.js
+++ script/make_task.js
@@ -24,116 +24,123 @@ function fetchTaskFunction() {
         if (this.readyState == 4) {
             if (this.status == 200) {
                 const objects = JSON.parse(this.responseText);
+                if (objects.length === 0) {
+                    document.getElementById("table").style.display = "none";
+                    document.getElementById("share").style.display = "none";
+                    document.getElementById("notify").innerHTML = "Your task is empty! Start to make some by press the button below";
+                } else {
+                    for (let list of objects) {
+                        counter++;
 
-                for (let list of objects) {
-                    counter++;
-                    var tokens = ['Apple', 'Banana', 'The', 'Task'];
-                    var text = '';
-                    for (var i = 0; i < 11; i++) {
-                        text += tokens[Math.floor(Math.random() * tokens.length)];
-                    }
+                        //create random text for td detail test
+                        var tokens = ['Apple', 'Banana', 'The', 'Task'];
+                        var text = '';
+                        for (var i = 0; i < 11; i++) {
+                            text += tokens[Math.floor(Math.random() * tokens.length)];
+                        }
 
-                    const task_lists = document.getElementById("task_lists");
+                        const task_lists = document.getElementById("task_lists");
 
-                    const tr = document.createElement("tr");
-                    const th_id = document.createElement("th");
-                    const td_name = document.createElement("td");
-                    const td_status = document.createElement("td");
-                    const td_deadline = document.createElement("td");
-                    const td_details = document.createElement("td");
-                    const td_options = document.createElement("td");
+                        const tr = document.createElement("tr");
+                        const th_id = document.createElement("th");
+                        const td_name = document.createElement("td");
+                        const td_status = document.createElement("td");
+                        const td_deadline = document.createElement("td");
+                        const td_details = document.createElement("td");
+                        const td_options = document.createElement("td");
 
-                    const edit_button = document.createElement("button");
-                    const completed_button = document.createElement("button");
-                    const move_button = document.createElement("button");
-                    const delete_button = document.createElement("button");
+                        const edit_button = document.createElement("button");
+                        const completed_button = document.createElement("button");
+                        const move_button = document.createElement("button");
+                        const delete_button = document.createElement("button");
 
-                    const edit_i = document.createElement("i");
-                    const completed_i = document.createElement("i");
-                    const move_i = document.createElement("i");
-                    const delete_i = document.createElement("i");
+                        const edit_i = document.createElement("i");
+                        const completed_i = document.createElement("i");
+                        const move_i = document.createElement("i");
+                        const delete_i = document.createElement("i");
 
-                    th_id.scope = "row";
+                        th_id.scope = "row";
 
-                    edit_button.className = "btn btn-primary";
-                    edit_button.title = "Edit";
-                    edit_button.setAttribute("data-toggle", "modal");
-                    edit_button.setAttribute("data-target", "#editModal");
-                    edit_i.className = "fa fa-pencil-square-o";
+                        edit_button.className = "btn btn-primary";
+                        edit_button.title = "Edit";
+                        edit_button.setAttribute("data-toggle", "modal");
+                        edit_button.setAttribute("data-target", "#editModal");
+                        edit_i.className = "fa fa-pencil-square-o";
 
-                    completed_button.className = "btn btn-success";
-                    completed_button.title = "Mark as completed";
-                    completed_button.setAttribute("data-toggle", "modal");
-                    completed_button.setAttribute("data-target", "#completedModal");
-                    completed_i.className = "fa fa-check-square-o";
+                        completed_button.className = "btn btn-success";
+                        completed_button.title = "Mark as completed";
+                        completed_button.setAttribute("data-toggle", "modal");
+                        completed_button.setAttribute("data-target", "#completedModal");
+                        completed_i.className = "fa fa-check-square-o";
 
-                    move_button.className = "btn btn-warning";
-                    move_button.title = "Move to another folder";
-                    move_button.setAttribute("data-toggle", "modal");
-                    move_button.setAttribute("data-target", "#moveModal");
-                    move_i.className = "fa fa-exchange";
+                        move_button.className = "btn btn-warning";
+                        move_button.title = "Move to another folder";
+                        move_button.setAttribute("data-toggle", "modal");
+                        move_button.setAttribute("data-target", "#moveModal");
+                        move_i.className = "fa fa-exchange";
 
-                    delete_button.className = "btn btn-danger";
-                    delete_button.title = "Delete";
-                    delete_button.setAttribute("data-toggle", "modal");
-                    delete_button.setAttribute("data-target", "#deletedModal");
-                    delete_i.className = "fa fa-trash-o";
+                        delete_button.className = "btn btn-danger";
+                        delete_button.title = "Delete";
+                        delete_button.setAttribute("data-toggle", "modal");
+                        delete_button.setAttribute("data-target", "#deletedModal");
+                        delete_i.className = "fa fa-trash-o";
 
-                    th_id.setAttribute("id", list["id"]);
+                        th_id.setAttribute("id", list["id"]);
 
-                    th_id.innerHTML = counter + 1;
-                    td_name.innerHTML = list["name"];
-                    if (list["done_count"] == 0) {
-                        td_status.innerHTML = "Not done";
-                    } else {
-                        td_status.innerHTML = "Done";
-                    }
+                        th_id.innerHTML = counter + 1;
+                        td_name.innerHTML = list["name"];
+                        if (list["done_count"] == 0) {
+                            td_status.innerHTML = "Not done";
+                        } else {
+                            td_status.innerHTML = "Done";
+                        }
 
-                    td_deadline.innerHTML = "dd/mm/yyyy";
-                    td_details.innerHTML = text;
+                        td_deadline.innerHTML = "dd/mm/yyyy";
+                        td_details.innerHTML = text;
 
-                    //create table
-                    task_lists.appendChild(tr);
-                    tr.appendChild(th_id);
-                    tr.appendChild(td_name);
-                    tr.appendChild(td_status);
-                    tr.appendChild(td_deadline);
-                    tr.appendChild(td_details);
-                    tr.appendChild(td_options);
+                        //create table
+                        task_lists.appendChild(tr);
+                        tr.appendChild(th_id);
+                        tr.appendChild(td_name);
+                        tr.appendChild(td_status);
+                        tr.appendChild(td_deadline);
+                        tr.appendChild(td_details);
+                        tr.appendChild(td_options);
 
-                    td_options.appendChild(edit_button);
-                    edit_button.appendChild(edit_i);
+                        td_options.appendChild(edit_button);
+                        edit_button.appendChild(edit_i);
 
-                    td_options.appendChild(completed_button);
-                    completed_button.appendChild(completed_i);
+                        td_options.appendChild(completed_button);
+                        completed_button.appendChild(completed_i);
 
-                    td_options.appendChild(move_button);
-                    move_button.appendChild(move_i);
+                        td_options.appendChild(move_button);
+                        move_button.appendChild(move_i);
 
-                    td_options.appendChild(delete_button);
-                    delete_button.appendChild(delete_i);
+                        td_options.appendChild(delete_button);
+                        delete_button.appendChild(delete_i);
 
-                    const btn = document.getElementsByClassName("btn btn-danger")[counter];
-                    btn.addEventListener('click', function () {
-                        let confirm = document.getElementById('delete_btn');
-                        confirm.addEventListener('click', function () {
-                            xhttp.open("DELETE", `${serverUrl}/task_lists/${th_id.id}`);
-                            xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-                            xhttp.setRequestHeader("Access-Token", jwt);
-                            xhttp.setRequestHeader("Uid", uid);
-                            xhttp.setRequestHeader("Client", client);
-                            xhttp.send();
-                            Swal.fire({
-                                text: 'Task deleted! Reload webpage',
-                                icon: 'success',
-                                confirmButtonText: 'OK'
-                            }).then((result) => {
-                                if (result.isConfirmed) {
-                                    location.reload();
-                                }
-                            })
+                        const btn = document.getElementsByClassName("btn btn-danger")[counter];
+                        btn.addEventListener('click', function () {
+                            let confirm = document.getElementById('delete_btn');
+                            confirm.addEventListener('click', function () {
+                                xhttp.open("DELETE", `${serverUrl}/task_lists/${th_id.id}`);
+                                xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+                                xhttp.setRequestHeader("Access-Token", jwt);
+                                xhttp.setRequestHeader("Uid", uid);
+                                xhttp.setRequestHeader("Client", client);
+                                xhttp.send();
+                                Swal.fire({
+                                    text: 'Task deleted! Reload webpage',
+                                    icon: 'success',
+                                    confirmButtonText: 'OK'
+                                }).then((result) => {
+                                    if (result.isConfirmed) {
+                                        location.reload();
+                                    }
+                                })
+                            });
                         });
-                    });
+                    }
                 }
             }
         }
@@ -203,9 +210,3 @@ function addTaskFunction() {
         };
     }
 }
-
-
-
-
-
-
