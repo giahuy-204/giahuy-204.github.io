8456dc9178ddd7f2c3409f9eaaa47f7b1b4c016b
Huy Dang <giahuy.dng@gmail.com>
2021-12-31 00:50:27 +0700

    sort table alphabet and numeric

--- script/make_task.js
+++ script/make_task.js
@@ -72,6 +72,7 @@ function selectFolder() {
     document.getElementById("share").style.display = "inline";
     document.getElementById("btn_addtask").style.display = "inline";
     document.getElementById("notify").innerHTML = "";
+    document.getElementById("filter_select").value = "all";
     counter = 0;
     $("#task_lists").empty();
     fetchTask();
@@ -537,6 +538,80 @@ function filterStatus() {
             }
         } else {
             rows[i].style.display = "table-row";
+        }
+    }
+}
+
+function sortTableNumerically() {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("table");
+    switching = true;
+    dir = "asc";
+    while (switching) {
+        switching = false;
+        rows = table.rows;
+        for (i = 1; i < (rows.length - 1); i++) {
+            shouldSwitch = false;
+            x = rows[i].getElementsByTagName("th")[0];
+            y = rows[i + 1].getElementsByTagName("th")[0];
+            if (dir == "asc") {
+                if (parseInt(x.innerHTML) > parseInt(y.innerHTML)) {
+                    shouldSwitch = true;
+                    break;
+                }
+            } else if (dir == "desc") {
+                if (parseInt(x.innerHTML) < parseInt(y.innerHTML)) {
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+        }
+        if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            switchcount++;
+        } else {
+            if (switchcount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
+            }
+        }
+    }
+}
+
+function sortTableAlphabetical(n) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("table");
+    switching = true;
+    dir = "asc";
+    while (switching) {
+        switching = false;
+        rows = table.rows;
+        for (i = 1; i < (rows.length - 1); i++) {
+            shouldSwitch = false;
+            x = rows[i].getElementsByTagName("td")[n];
+            y = rows[i + 1].getElementsByTagName("td")[n];
+            if (dir == "asc") {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    shouldSwitch = true;
+                    break;
+                }
+            } else if (dir == "desc") {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+        }
+        if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            switchcount++;
+        } else {
+            if (switchcount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
+            }
         }
     }
 }
